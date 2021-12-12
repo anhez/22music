48618ece8d832499a7a0a09efdd342057248f9cf
anhez <anhtd.dev@gmail.com>
2021-12-12 14:55:48 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+
+function App() {
+	const [count, setCount] = useState(0)
+
+	var aaaa = 19
+	const a = 10
+	const atom = {
+            value: 1,
+            addValue: function (value: string) {
+                return atom.value + value
+            },
+	}
+	const item = new Object()
+	return (
+		<div className="app">
+			<header className="header">this is header</header>
+			<div className="body">
+				<div className="sidebar">this is side bar</div>
+				<div className="content">this is content area</div>
+			</div>
+		</div>
+	)
+}
+
+export default App
