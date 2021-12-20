665cd4bb5b98b4ebccc6aac57824b0283ced01f2
anhez <anhtd.dev@gmail.com>
2021-12-20 21:39:27 +0700

    pre commit hook 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import { useState } from "react"
-import logo from "./logo.svg"
 import "./App.css"
 
 const App = function () {
 	const [count, setCount] = useState(0)
 
-	const aaaa = 19
-	const a = 10
 	const atom = {
 		value: 1,
-		addValue(value: string) {
-			return atom.value + value
-		},
 	}
-	const b = 1
-	console.log(logo, count, setCount, aaaa, a, item, b)
+	console.log(count, setCount, atom)
+
 	return (
 		<div className="app">
 			<header className="header">this is header</header>
